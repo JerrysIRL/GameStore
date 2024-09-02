4daaf382075c3b96088e90bd1383874c4b9e7ca1
JerrysIRL <sergei.maltcev@edu.futuregames.nu>
2024-09-02 11:15:04 +0200

    FrontEnd push

---
+++ gamestore.frontend/Client/GamesClient.cs
@@ -0,0 +1,30 @@
+﻿using GameStore.Frontend.Models;
+
+namespace GameStore.Frontend.Client;
+
+public class GamesClient(HttpClient httpClient)
+{
+    public async Task<GameSummary[]> GetGamesAsync() =>
+        await httpClient.GetFromJsonAsync<GameSummary[]>("/games") ?? [];
+
+    public async Task AddGameAsync(GameDetails game)
+    {
+        await httpClient.PostAsJsonAsync("games", game);
+    }
+
+    public async Task UpdateGameAsync(GameDetails gameDetails)
+    {
+        await httpClient.PutAsJsonAsync($"games/{gameDetails.Id}", gameDetails);
+    }
+
+    public async Task<GameDetails> GetGameAsync(int id)
+    {
+        return await httpClient.GetFromJsonAsync<GameDetails>($"games/{id}") ?? throw new Exception("Game not found");
+    }
+
+    public async Task DeleteGame(int id)
+    {
+        await httpClient.DeleteAsync($"games/{id}");
+    }
+
+}
---
+++ gamestore.frontend/Client/GenreClient.cs
@@ -0,0 +1,9 @@
+﻿using GameStore.Frontend.Models;
+
+namespace GameStore.Frontend.Client;
+
+public class GenreClient(HttpClient httpClient)
+{
+    public async Task<Genre[]> GetGenresAsync() => await httpClient.GetFromJsonAsync<Genre[]>("genres")
+                                              ?? throw new Exception("Genres not found");
+}
